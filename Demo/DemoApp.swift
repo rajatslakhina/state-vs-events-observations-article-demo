17cae9a9f11ced634022cbb79e5614e5638aae58
Rajat S. Lakhina <130336875+rajatslakhina@users.noreply.github.com>
2026-07-28 13:10:50 +0530

    Add Demo app entry point

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import ObservationsMigrationKit
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ScannerDemoView()
+        }
+    }
+}
